ecb7f2348e80d9a3766f3e9ec8a52a0c72a01b5b
cindyyzhu <cindyzhu8207@gmail.com>
2024-08-03 13:43:11 -0400

    auth

---
+++ sharity/app/components/LoginPrompt.js
@@ -0,0 +1,13 @@
+import "../dashboard.css";
+import MatchingButton from "./MatchingButton";
+
+function LoginPrompt() {
+  return (
+    <div className="m-2 w-full signup-prompt">
+      <h3>Sign up or log in to get started.</h3>
+      <a href="/api/auth/login">Login</a>     
+    </div>
+  )
+}
+
+export default LoginPrompt;
--- sharity/app/page.js
+++ sharity/app/page.js
@@ -1,7 +1,26 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import "./index.css";
+import LoginPrompt from "./components/LoginPrompt";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useRouter } from "next/navigation";
+
+function Dashboard() {
+
+  const { user, error, loading } = useUser();
+  const router = useRouter();
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <h1>Error with auth</h1>
+
+  if (user) router.push("home");
+
   return (
-    <>Hello World</>
+    <div className="w-full dashboard">
+
+      <LoginPrompt />
+    </div>
   );
 }
+
+export default Dashboard;
