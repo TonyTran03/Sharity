fba70d2a8871314d53f746ac09d0fff84ee59912
cindyyzhu <cindyzhu8207@gmail.com>
2024-08-03 19:06:51 -0400

    formatted the nav bar buttons

--- sharity/app/components/Navbar.jsx
+++ sharity/app/components/Navbar.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import MatchingButton from './MatchingButton';
+import NavBarButton from './NavBarButton';
 import "../globals.css";
 import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <div className="relative">
-      <div className="navbar bg-base-100 flex items-center p-10">
-        <div className="flex-1 flex justify-center space-x-10">
-
-          <Link href="/profile">
-          <button className="button">Profile</button>
-          </Link>
-          <button className="button">Charity Map 🗺️</button>
-
-
-          <button className="button">Charity Search 🔎</button>
-          <div className="fixed top-10 right-10 flex justify-center space-x-10 z-50">
-        <MatchingButton link="/api/auth/logout">Logout</MatchingButton>
-        </div>
-        
-      </div>
-      </div>
-      
+      <NavBarButton link="/profile">Profile</NavBarButton>
+      <NavBarButton link="/map">Charity Map 🗺️</NavBarButton>
+      <NavBarButton link = "/search">Charity Search 🔎</NavBarButton>
+      <MatchingButton link="/api/auth/logout">Logout</MatchingButton>
     </div>
   );
 };
