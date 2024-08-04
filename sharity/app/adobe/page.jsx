9118e89eb16d323db712d43351f5fc3eed9c4bd0
cindyyzhu <cindyzhu8207@gmail.com>
2024-08-04 03:01:20 -0400

    added adobe and client id

--- sharity/app/adobe/page.jsx
+++ sharity/app/adobe/page.jsx
@@ -1,22 +1,40 @@
 'use client'
+import { useEffect } from 'react';
 import Head from 'next/head';
-import AdobePDFEmbed from './AdobeEmbeded.js';
 
 const Home = () => {
-    const pdfUrl = 'https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:a26a0f72-e63e-4de7-a1fc-cb280961ebdc';
-    const fileName = 'Embedded PDF Document';
-  
+    useEffect(() => {
+        // Load the Adobe PDF Embed API script dynamically
+        const script = document.createElement('script');
+        script.src = "https://acrobatservices.adobe.com/view-sdk/viewer.js";
+        script.async = true;
+        document.body.appendChild(script);
+
+        script.onload = () => {
+            // Initialize Adobe DC View SDK after the script has loaded
+            document.addEventListener("adobe_dc_view_sdk.ready", function () { 
+                const adobeDCView = new AdobeDC.View({ clientId: "a6881fc0aa2749b888d30a5ff3a5baf4", divId: "adobe-dc-view" });
+                adobeDCView.previewFile({
+                    content: { location: { url: "https://acrobatservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf" } },
+                    metaData: { fileName: "Bodea Brochure.pdf" }
+                }, {});
+            });
+        };
+
+        return () => {
+            // Clean up the script when the component unmounts
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
-      <>
-        <Head>
-          <title>Adobe PDF Embed</title>
-        </Head>
-        <main>
-          <h1>Adobe PDF Embed API Sample</h1>
-          <AdobePDFEmbed pdfUrl={pdfUrl} fileName={fileName} />
-        </main>
-      </>
+        <div>
+            <Head>
+                <title>Embedded PDF Document</title>
+            </Head>
+            <div id="adobe-dc-view" style={{ height: '100vh' }}></div>
+        </div>
     );
-  };
-  
-  export default Home;
+};
+
+export default Home;
