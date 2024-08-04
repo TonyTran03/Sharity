b56949e1b29c0011bbc2f6aa215c48aee5e08ef7
karenzhang0701 <109870617+karenzhang0701@users.noreply.github.com>
2024-08-03 21:41:05 -0400

    updated homepage

--- sharity/app/home/page.jsx
+++ sharity/app/home/page.jsx
@@ -6,6 +6,26 @@ function SharityLogo() {
   return <img src={"Sharity.png"} alt="Sharity Logo"/>;
 }
 
+export function BulletPointList() {
+
+  return (
+    <div>
+      <ul style={{ listStyleType: 'disc', paddingLeft: '30px' }}>
+      <li style={{ marginBottom: '10px', paddingLeft: '10px' }}>
+        <span style={{ fontWeight: 'bold', color: '#c77593' }}>Live Map:</span> see what donations charities need
+      </li>
+
+        <li style={{ marginBottom: '10px' , padding: '10px'}}>
+          <span style={{ fontWeight: 'bold', color: '#c77593' }}>Track</span> your donations
+        </li>
+
+        <li style={{ marginBottom: '10px', padding: '10px'}}>
+          <span style={{ fontWeight: 'bold', color: '#c77593' }}>Optimize Resources:</span> charities can see each other's stocks, helping them work better together</li>
+      </ul>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div>
@@ -13,7 +33,8 @@ export default function Home() {
         <div className="side-by-side">
           <SharityLogo />
           <div className="rectangle" style={{width: '400px', height: '400px'}}>
-          <h1 style={{ fontSize: '20px', marginBottom: '20px', paddingTop:'60px', fontWeight:'bold'}}>Connecting charities and communities</h1>
+          <h1 style={{ fontSize: '20px', marginBottom: '20px', paddingTop:'50px', fontWeight:'bold',  color: '#c77593', padding: '20px'}}>Connecting charities and communities to streamline donations</h1>
+          <BulletPointList/>
           </div>
         </div>
       </div>
