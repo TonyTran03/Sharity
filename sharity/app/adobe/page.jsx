2dde0e691a7e2b025ee4169be62a3471a96d0b33
cindyyzhu <cindyzhu8207@gmail.com>
2024-08-04 04:29:08 -0400

    adobe

--- sharity/app/adobe/page.jsx
+++ sharity/app/adobe/page.jsx
@@ -23,7 +23,7 @@ const Home = () => {
                 const adobeDCView = new AdobeDC.View({ clientId: "a6881fc0aa2749b888d30a5ff3a5baf4", divId: "adobe-dc-view" });
                 adobeDCView.previewFile({
                     content: { location: { url: "https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:a26a0f72-e63e-4de7-a1fc-cb280961ebdc" } },
-                    metaData: { fileName: "Get Started With Sharity Tutorial.pdf" }
+                    metaData: { fileName: "Sharity.pdf" }
                 }, {});
             });
         };
